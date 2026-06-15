3487c09949173822fc940e05fa6942ea45d27845
Dawid Dominiak <dawid.dominiak@pgnig.pl>
2026-06-15 13:21:40 +0200

    Dodanie prettiera

--- tests/equation.test.js
+++ tests/equation.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+const assert = require("assert");
 
 function resolveParams(targetValue) {
   return targetValue * (10 % 4);
@@ -6,9 +6,9 @@ function resolveParams(targetValue) {
 
 function runTests() {
   const result = resolveParams(132.993);
-  assert.strictEqual(result, 265.986, 'Expected params to equal 265.986');
+  assert.strictEqual(result, 265.986, "Expected params to equal 265.986");
 
-  console.log('PASS: resolveParams returns correct value');
+  console.log("PASS: resolveParams returns correct value");
 }
 
 runTests();
